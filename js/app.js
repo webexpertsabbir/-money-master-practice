172c0091769d506b007a8493953afdddd751dfcc
Sabbir <webexpertsabbir@gmail.com>
2022-08-20 15:06:32 +0600

    calculation

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
-function getCalculatePrice(inoutId){
-    const inputeField = document.getElementById(inoutId);
+function getCalculatePrice(inputId){
+    const inputeField = document.getElementById(inputId);
     const inputeFieldStiring = inputeField.value;
-    const carentInputField = parseFloat(inputeFieldStiring);
+    console.log(typeof inputeFieldStiring);
+
+    const carentInputField = parseInt(inputeFieldStiring);
     inputeField.value = '';
     return carentInputField;
 
 }
-function getIncome(inoutId){
-    const inputeField = document.getElementById(inoutId);
+function getIncome(inputId){
+    const inputeField = document.getElementById(inputId);
     const inputeFieldStiring = inputeField.value;
     const carentInputField = parseFloat(inputeFieldStiring);
     return carentInputField;
@@ -20,16 +22,35 @@ function setCalculatePrice(element, newValue){
 }
 
 document.getElementById('btn-calculate').addEventListener('click', function(){
-    const foodPrice = getCalculatePrice('food-price');
-    const rantPrice = getCalculatePrice('rent-price');
-    const clothesPrice = getCalculatePrice('clothes-price');
-    
-    const totalPrice = clothesPrice + rantPrice + foodPrice;
-    setCalculatePrice('total-expenses', totalPrice);
 
-    const previousBlance = getIncome('total-income');
-    const carentBlance = previousBlance - totalPrice;
-    setCalculatePrice('balance', carentBlance)
+    const foodPrice1 = document.getElementById('food-price').value;
+    const rantPrice1 = document.getElementById('rent-price').value;
+    const clothesPrice1 = document.getElementById('clothes-price').value;
+    if(isNaN(foodPrice1) == true || isNaN(rantPrice1) == true  || isNaN(clothesPrice1) == true) {
+        alert('pleace input a number')
+    }
+    else{
+        const foodPrice = getCalculatePrice('food-price');
+        const rantPrice = getCalculatePrice('rent-price');
+        const clothesPrice = getCalculatePrice('clothes-price');
+        if(foodPrice < 0 || rantPrice < 0 || clothesPrice < 0){
+            alert('Pleace inter posetive value')
+            return;
+        }
+        const totalPrice = clothesPrice + rantPrice + foodPrice;
+        const previousBlance = getIncome('total-income');
+        const carentBlance = previousBlance - totalPrice;
+        if(totalPrice > previousBlance){
+            alert('apner taka kom ache')
+            return;
+        }
+        else if(isNaN(carentBlance) == true){
+            alert('apni input den ni');
+            return;
+        }
+        setCalculatePrice('total-expenses', totalPrice);
+        setCalculatePrice('balance', carentBlance)
+    }
 
 })
 
